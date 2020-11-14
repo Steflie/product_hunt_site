3204fad23f97e75e2bbf98b1ef058875b50d43ab
Steflie <mccauchy@yahoo.com>
2020-11-15 00:22:37 +0200

    Second Commit

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -7,5 +7,6 @@ from product_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
-    path('accounts/', include('account_app.urls'))
+    path('accounts/', include('account_app.urls')),
+    path('products/', include('product_app.urls')),
 ]
